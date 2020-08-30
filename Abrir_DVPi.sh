6bc6e0321c95c3c5045ca6c60cea4cef182e54b8
Manel <ea3eiz@ea3eiz.com>
2020-08-30 21:54:09 +0200

    1

--- Abrir_DVPi.sh
+++ Abrir_DVPi.sh
@@ -3,17 +3,17 @@
 usuario=$(awk "NR==1" /home/pi/.config/autostart/usuario)
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 
-# cd /home/pi/Desktop
-# sudo cp Abrir_dv4mini.desktop /home/pi
-# sed -i "4c DV4mini=ON" /home/pi/status.ini
-# sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_dv4mini.sh'" /home/pi/Abrir_dv4mini.desktop
-# sed -i "5c Icon=/home/pi/$SCRIPTS_version/DV4_ON.png" /home/pi/Abrir_dv4mini.desktop
-# sed -i "10c Name[es_ES]=Cerrar DV4mini" /home/pi/Abrir_dv4mini.desktop
-# sed -i "4c DV4mini=ON" /home/pi/status.ini
-# cd /home/pi
-# sudo cp Abrir_dv4mini.desktop /home/pi/Desktop
-# sleep 1
-# sudo rm /home/pi/Abrir_dv4mini.desktop
+cd $usuario/Desktop
+sudo cp Abrir_DVPi.desktop /home/pi
+sed -i "4c DV4mini=ON" $usuario/status.ini
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh Cerrar_DVPi.sh'" $usuario/Abrir_DVPi.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DVPI_ON.png" $usuario/Abrir_DVPi.desktop
+sed -i "10c Name[es_ES]=Cerrar DV4mini" $usuario/Abrir_DVPi.desktop
+sed -i "4c DV4mini=ON" $usuario/status.ini
+cd $usuario
+sudo cp Abrir_DVPi.desktop $usuario/Desktop
+sleep 1
+sudo rm $usuario/Abrir_DVPi.desktop
 
 cd /home/pi/dvpi
 mono dvpi
