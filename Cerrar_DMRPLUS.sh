629927a16f63628a71aa53b079dbf844c801363b
Manel <ea3eiz@ea3eiz.com>
2020-09-01 00:08:37 +0200

    1

--- Cerrar_DMRPLUS.sh
+++ Cerrar_DMRPLUS.sh
@@ -4,13 +4,13 @@ killall MMDVMPLUS
 usuario=$(awk "NR==1" /home/pi/.config/autostart/usuario)
 SCRIPTS_version=$(awk "NR==1" $usuario/.config/autostart/version)
 
-sed -i "4c Exec=sh -c 'cd $usuario/$SCRIPTS_version;lxterminal --geometry=80x12 -e sudo sh Abrir_DMRPLUS.sh'" $usuario/Abrir_DMRPLUS.desktop
-sed -i "5c Icon=$usuario/$SCRIPTS_version/ICONO_DMRPLUS_OFF.png" $usuario/Abrir_DMRPLUS.desktop
-sed -i "10c Name[es_ES]=Abrir DMR+" $usuario/Abrir_DMRPLUS.desktop
+sed -i "4c Exec=sh -c 'cd $usuario/$SCRIPTS_version;lxterminal --geometry=80x12 -e sudo sh Abrir_DMRPLUS.sh'" $usuario/Abrir_MMDVMPLUS.desktop
+sed -i "5c Icon=$usuario/$SCRIPTS_version/ICONO_DMRPLUS_OFF.png" $usuario/Abrir_MMDVMPLUS.desktop
+sed -i "10c Name[es_ES]=Abrir DMR+" $usuario/Abrir_MMDVMPLUS.desktop
 sed -i "6c MMDVMPLUS=OFF" $usuario/status.ini
 
 cd $usuario
-sudo cp Abrir_DMRPLUS.desktop $usuario/Desktop
+sudo cp Abrir_MMDVMPLUS.desktop $usuario/Desktop
 
-sudo rm $usuario/Abrir_DMRPLUS.desktop
+sudo rm $usuario/Abrir_MMDVMPLUS.desktop
 
