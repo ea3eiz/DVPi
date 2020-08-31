c04f38d39e4cee227264f3f3e784043209d9395c
Manel <ea3eiz@ea3eiz.com>
2020-09-01 00:11:42 +0200

    1

--- Cerrar_DMRPLUS.sh
+++ Cerrar_DMRPLUS.sh
@@ -6,7 +6,7 @@ SCRIPTS_version=$(awk "NR==1" $usuario/.config/autostart/version)
 
 sed -i "4c Exec=sh -c 'cd $usuario/$SCRIPTS_version;lxterminal --geometry=80x12 -e sudo sh Abrir_DMRPLUS.sh'" $usuario/Abrir_MMDVMPLUS.desktop
 sed -i "5c Icon=$usuario/$SCRIPTS_version/ICONO_DMRPLUS_OFF.png" $usuario/Abrir_MMDVMPLUS.desktop
-sed -i "10c Name[es_ES]=Abrir DMR+" $usuario/Abrir_MMDVMPLUS.desktop
+sed -i "10c Name[es_ES]=Abrir DMR+" $usuario/$SCRIPTS_version/Abrir_MMDVMPLUS.desktop
 sed -i "6c MMDVMPLUS=OFF" $usuario/status.ini
 
 cd $usuario
