be575dcc187d0393a908d8b3ec70680a7bdebc7e
Manel <ea3eiz@ea3eiz.com>
2020-08-31 21:18:50 +0200

    1

--- Editar_DVPi.sh
+++ Editar_DVPi.sh
@@ -72,8 +72,8 @@ echo -n "${CIAN}   8)${GRIS} Modificar Options     - ${AMARILLO}"
 options_BM=$(awk "NR==77" /opt/MMDVM_Bridge/MMDVM_Bridge.ini)
 echo "$options_BM"
 
-echo -n "${CIAN}   G)${GRIS} Graba                 - ${AMARILLO}"
-echo "Y reinicia ${CIAN}$DIRECTORIO"
+echo -n "${CIAN}   G)${GRIS} Graba estos datos en  - ${AMARILLO}"
+echo "${CIAN}$DIRECTORIO ${GRIS}y reinicia el sistema"
 
 
 if [ $sistema = Port=55555 ]
