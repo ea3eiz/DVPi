7385a668b1e2b04d7511f95e154abc188c518f29
Manel <ea3eiz@ea3eiz.com>
2020-09-07 13:09:53 +0200

    1

--- actualizar.sh
+++ actualizar.sh
@@ -8,13 +8,13 @@ xfdesktop --reload
 # Comprueba si DVSWITCH está activado
 estado_dvswitch=$(awk "NR==18" /home/pi/status.ini)
 if [ "$estado_dvswitch" = 'DVSWITCH=OFF' ];then
-sudo systemctl stop ysfgateway.service
-sudo systemctl stop dmr2ysf.service
-sudo systemctl stop analog_bridge.service
-sudo systemctl stop ircddbgateway.service
-sudo systemctl stop md380-emu.service
-sudo systemctl stop mmdvm_bridge.service
-sudo systemctl stop nxdngateway.service
+# sudo systemctl stop ysfgateway.service
+# sudo systemctl stop dmr2ysf.service
+# sudo systemctl stop analog_bridge.service
+# sudo systemctl stop ircddbgateway.service
+# sudo systemctl stop md380-emu.service
+# sudo systemctl stop mmdvm_bridge.service
+# sudo systemctl stop nxdngateway.service
 else
 echo ""
 fi
@@ -260,13 +260,13 @@ sleep 2
 # Comprueba si DVSWITCH está activado
 estado_dvswitch=$(awk "NR==18" /home/pi/status.ini)
 if [ "$estado_dvswitch" = 'DVSWITCH=OFF' ];then
-sudo systemctl stop ysfgateway.service
-sudo systemctl stop dmr2ysf.service
-sudo systemctl stop analog_bridge.service
-sudo systemctl stop ircddbgateway.service
-sudo systemctl stop md380-emu.service
-sudo systemctl stop mmdvm_bridge.service
-sudo systemctl stop nxdngateway.service
+# sudo systemctl stop ysfgateway.service
+# sudo systemctl stop dmr2ysf.service
+# sudo systemctl stop analog_bridge.service
+# sudo systemctl stop ircddbgateway.service
+# sudo systemctl stop md380-emu.service
+# sudo systemctl stop mmdvm_bridge.service
+# sudo systemctl stop nxdngateway.service
 else
 echo ""
 fi
