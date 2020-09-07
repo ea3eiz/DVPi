2aeeabf69af1337d8af3e5322425b1dad583b9b1
Manel <ea3eiz@ea3eiz.com>
2020-09-07 14:32:36 +0200

    1

--- Abrir_DVPi.sh
+++ Abrir_DVPi.sh
@@ -9,7 +9,7 @@ sed -i "4c DV4mini=ON" $usuario/status.ini
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh Cerrar_DVPi.sh'" $usuario/Abrir_DVPi.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DVPI_ON.png" $usuario/Abrir_DVPi.desktop
 sed -i "10c Name[es_ES]=Cerrar DVPi" $usuario/Abrir_DVPi.desktop
-sed -i "4c DV4mini=ON" $usuario/status.ini
+sed -i "22c DVPi=ON" $usuario/status.ini
 cd $usuario
 sudo cp Abrir_DVPi.desktop $usuario/Desktop
 sleep 1
--- Cerrar_DVPi.sh
+++ Cerrar_DVPi.sh
@@ -9,7 +9,7 @@ sed -i "4c DV4mini=ON" $usuario/status.ini
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh Abrir_DVPi.sh'" $usuario/Abrir_DVPi.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DVPI_OFF.png" $usuario/Abrir_DVPi.desktop
 sed -i "10c Name[es_ES]=Abrir DVPi" $usuario/Abrir_DVPi.desktop
-sed -i "4c DV4mini=ON" $usuario/status.ini
+sed -i "22c DVPi=OFF" $usuario/status.ini
 cd $usuario
 sudo cp Abrir_DVPi.desktop $usuario/Desktop
 sleep 1
