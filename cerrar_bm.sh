cb1b616dca8b7f9012dddbf4661e35ebc63b89ed
Manel <ea3eiz@ea3eiz.com>
2021-08-20 22:15:52 +0200

    1

--- cerrar_bm.sh
+++ cerrar_bm.sh
@@ -9,7 +9,7 @@ sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_BM_OFF.png" /home/pi/Abrir_MMDVM
 sed -i "10c Name[es_ES]=Abrir BM" /home/pi/Abrir_MMDVMBM.desktop
 sed -i "7c MMDVMBM=OFF" /home/pi/status.ini
 cd /home/pi
-
+sleep 2
 sudo cp Abrir_MMDVMBM.desktop /home/pi/Desktop
 
 sudo rm /home/pi/Abrir_MMDVMBM.desktop
